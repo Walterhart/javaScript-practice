4530621c7fa6e00e9a04ac7a1f6537ee5804036d
Walterhart <70658679+Walterhart@users.noreply.github.com>
2023-01-14 16:06:15 -0500

    Add  functionality  for increment, reset and decrement. Add button save

--- passenger-counter-app/index.js
+++ passenger-counter-app/index.js
@@ -1,6 +1,33 @@
-function increment(){
 
+// global
+// DOM document object model: how you use js to modify website
+let counter = document.getElementById("counter") 
+
+let count = 0 
+
+
+// increase count
+function increment(){
+    count = count + 1
+    counter.innerText = count
+    console.log(count)
 }
+
+// decrease count
 function decrement(){
-    
+    count = count - 1
+    counter.innerText = count
+    console.log(count)
+}
+
+// reset count
+function reset(){
+    count = 0;
+    counter.innerText = count
+    console.log(count)
+}
+
+// save
+function save(){
+    console.log(count)
 }
