51edcd29aead3105f6d8a149f6117fe71a1118f4
Walterhart <70658679+Walterhart@users.noreply.github.com>
2023-01-14 15:25:58 -0500

    Update log-sum.js

--- simple-practice/log-sum.js
+++ simple-practice/log-sum.js
@@ -1,12 +1,12 @@
 // Create a function that logs out the sum of all the lap times
 
-function logSum(lap){
-    console.log(lap)
+function logSum(lap1,lap2,lap3){
+    let totalLap= lap1+lap2+lap3
+    console.log(totalLap)
 }
 let lap1 = 34
 let lap2 = 33
 let lap3 = 36
-logSum(lap1)
-logSum(lap2)
-logSum(lap3)
+logSum(lap1, lap2, lap3)
+
 
