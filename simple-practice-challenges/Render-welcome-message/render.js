4fac9503f552ae94a81116c45d594e0eb3b2d4fb
Walterhart <70658679+Walterhart@users.noreply.github.com>
2023-01-14 16:31:24 -0500

    Add simple welcome challenge

---
+++ simple-practice-challenges/Render-welcome-message/render.js
@@ -0,0 +1,11 @@
+// Grab the welcome paragraph and store it in a variable
+let welcome = document.getElementById("welcome-msg")
+console.log(welcome)
+// create two variables (name and greeting) contains your name
+ let name = "Wally"
+ let greeting ="Hello "
+ 
+ let welcomeMessaeg = greeting + name
+// render the welcome message
+
+welcome.innerText= welcomeMessaeg
