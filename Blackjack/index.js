b4e49ea3f88273d27a737f8bf762ad91f1b43699
Walterhart <70658679+Walterhart@users.noreply.github.com>
2023-01-15 14:06:11 -0500

    Add basic random number generator  and conditions  for winning, drawing and losing

---
+++ Blackjack/index.js
@@ -0,0 +1,20 @@
+
+// this return only non-decimals number between 2-11
+let firstCard = Math.floor(Math.random() * 11) + 2;
+let secondCard = Math.floor(Math.random() * 11) + 2;
+let sum = firstCard + secondCard 
+console.log(firstCard + " + " + secondCard)
+console.log("Total:" + sum)
+
+if( sum < 21){
+    console.log("Draw another card?")
+}
+else if (sum === 21){
+    console.log("You win")
+}
+else {
+    console.log("Burst, you lost")
+}
+
+
+
