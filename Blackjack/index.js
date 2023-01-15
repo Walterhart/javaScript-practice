2cde3c442f0ce914f47425d21368ac1751871909
Walterhart <70658679+Walterhart@users.noreply.github.com>
2023-01-15 14:15:30 -0500

    Complete  a simple if challenge

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,6 +1,6 @@
 
 // this return only non-decimals number between 2-11
-let firstCard = Math.floor(Math.random() * 11) + 2;
+let firstCard = Math.floor(Math.random() * 13) + 2;
 let secondCard = Math.floor(Math.random() * 11) + 2;
 let sum = firstCard + secondCard 
 console.log(firstCard + " + " + secondCard)
