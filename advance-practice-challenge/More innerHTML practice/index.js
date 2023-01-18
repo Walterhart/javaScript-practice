d9969a5b047bfa1e8f0cf4c472d254ed2d5f45d1
Walterhart <70658679+Walterhart@users.noreply.github.com>
2023-01-18 17:03:30 -0500

    Complete another innerHTML challenge

--- advance-practice-challenge/More innerHTML practice/index.js
+++ advance-practice-challenge/More innerHTML practice/index.js
@@ -1,8 +1,11 @@
 
 const container = document.getElementById("container")
 
-container.innerHTML = "<button>Buy!</button>"
+container.innerHTML = "<button onclick='buy()'>Buy</button>"
 
 // When clicked, render a paragraph under the button (in the container)
 // that says "Thank you for buying!"
-container.innerHTML += "<p>"+ "Thank you for buying!" + "</p>"
+function buy(){
+    container.innerHTML +="<p>"+"Thank you for buying!"+"</p>"
+    console.log("bought")
+}
