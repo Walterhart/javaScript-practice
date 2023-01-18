98ae94a04ca6fa07a2e07fe94d95e4633d856b85
Walterhart <70658679+Walterhart@users.noreply.github.com>
2023-01-18 16:18:18 -0500

    Display links as list

--- Mark-extension/index.js
+++ Mark-extension/index.js
@@ -1,11 +1,17 @@
 
-let inputBtn = document.getElementById("input-btn")
-
-
+const inputBtn = document.getElementById("input-btn")
+let links = ["test.com","idk"]
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
 // listen for clicks from id input-btn
 // perform function on click
 // let js handle event listener instead of html
-inputBtn.addEventListener("click", function (){
-    console.log("clicked ")
+inputBtn.addEventListener("click",function (){
+    links.push(inputEl.value)
+    console.log(links)
 })
 
+for(i=0; i < links.length; i++){
+    console.log(links[i])
+    ulEl.innerHTML += "<li>"+ links[i] + "</li>"
+}
