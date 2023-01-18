10e801ccc875a7f941b13ee41a303b8848f97453
Walterhart <70658679+Walterhart@users.noreply.github.com>
2023-01-18 15:30:50 -0500

    Add event listener and complete addEventListener challenge

---
+++ Mark-extension/index.js
@@ -0,0 +1,11 @@
+
+let inputBtn = document.getElementById("input-btn")
+
+
+// listen for clicks from id input-btn
+// perform function on click
+// let js handle event listener instead of html
+inputBtn.addEventListener("click", function (){
+    console.log("clicked ")
+})
+
---
+++ advance-practice-challenge/addEventListener()/index.js
@@ -0,0 +1,9 @@
+// 1. Grab the box from the DOM and store it in a variable
+let boxEL = document.getElementById("box")
+// 2. Add a click event listener to the box 
+boxEL.addEventListener("click", function(){
+// 3. Log out "I want to open the box!" when it's clicked
+    console.log("I want to open the box!")
+})
+
+
