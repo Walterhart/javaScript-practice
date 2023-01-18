b1d8d594d00be9ccc7d412905d692b72bcd48819
Walterhart <70658679+Walterhart@users.noreply.github.com>
2023-01-18 13:44:27 -0500

    Complete object and function coding challenge

---
+++ simple-practice-challenges/Objects and function/index.js
@@ -0,0 +1,19 @@
+// Create a person object that contains three keys: name, age, and county.
+// Use yourself as an example to set the values for name, age, and country
+
+let person ={
+    name: "Wally",
+    age: 26,
+    country: "US",
+}
+// Create a function, logData(), that uses the person object to create a
+// string in the following format: 
+// "Per is 35 years old and lives in Norway"
+
+function logData(){
+    let data = person.name + " is " + person.age + " years old and lives in " + person.country
+    return data
+}
+// Call the logData() function to verify that it works
+let detail = logData()
+console.log(detail)
