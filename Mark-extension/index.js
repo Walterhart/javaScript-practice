82656e5420b7bcb81d96001172296d7c0e3bd4cd
Walterhart <70658679+Walterhart@users.noreply.github.com>
2023-01-23 16:03:24 -0500

    Save links to localstorage and add button to clear localstorage, dom and links

--- Mark-extension/index.js
+++ Mark-extension/index.js
@@ -1,17 +1,38 @@
 
 const inputBtn = document.getElementById("input-btn")
-let links = ["test.com","idk"]
+const deleteBTN = document.getElementById("delete-btn")
+let links = []
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
+
+// convert links from local storage into an array
+let storedLoacalStorageLinks = JSON.parse(localStorage.getItem("links"))
+console.log(storedLoacalStorageLinks)
+
+if(storedLoacalStorageLinks)
+{
+    links = storedLoacalStorageLinks
+    renderLinks()
+}
+
 
 
 // listen for clicks from id input-btn
 // perform function on click
 // let js handle event listener instead of html
+// convert links into string and store it in localstorage
 inputBtn.addEventListener("click",function (){
     links.push(inputEl.value)
-    console.log(links)
     inputEl.value =""
+    localStorage.setItem("links", JSON.stringify(links))
+    renderLinks()
+})
+
+// clear local storagem, links , and dom
+deleteBTN.addEventListener("dblclick", function(){
+    localStorage.clear()
+    links =[]
+
     renderLinks()
 })
 
@@ -21,14 +42,12 @@ function renderLinks(){
     // store links into listItems
     for(i=0; i < links.length; i++){
         
-        console.log(links[i])
         //listItems += "<li><a target = _blank href ='" + links[i]+"'>"+links[i] + "</a></li>"
         listItems += `
         <li>
             <a target =' _blank' href = '${links[i]}'> ${links[i]}  </a>
         </li>
         `
-        console.log(listItems)
     }
 
     // render links
